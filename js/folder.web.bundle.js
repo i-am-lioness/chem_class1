d01e321d6877fe1dfbc44f10b72e35f244609a32
nnennaude <nnenna.udegbunam@gmail.com>
2017-11-27 13:08:00 -0500

    Update 2017-11-27T18:07:59.101Z

--- js/folder.web.bundle.js
+++ js/folder.web.bundle.js
@@ -30304,14 +30304,21 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* eslint-env browser */
+/* global ADMIN */
 
 
 var _require = __webpack_require__(38),
     remote = _require.remote;
 
-var fs = remote.require('fs-extra');
-var path = remote.require('path');
-var push = remote.require('git-push');
+var fs = void 0;
+var path = void 0;
+var push = void 0;
+
+if (false) {
+  fs = remote.require('fs-extra');
+  path = remote.require('path');
+  push = remote.require('git-push');
+}
 
 var stages = {
   DEFAULT: 0,
